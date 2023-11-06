da242704a6e3f2ee4ff3f03cc7b58b7f7984815d
khadija ishaq <khadijaishaq151@gmail.com>
2023-11-06 22:53:24 +0200

    hello world revision

---
+++ hello-world/hello.js
@@ -0,0 +1,8 @@
+console.log("Hello I'm node js");
+
+// console.log(window) it'll give error but not in browser
+
+//console.log(alert("Hey"))
+
+//All DOM, window objects and UI work has been removed form node
+// npm init, defined a script
