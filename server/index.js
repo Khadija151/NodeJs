6ce15f591de48a13af0a0ef96321dc4e81610025
khadija ishaq <khadijaishaq151@gmail.com>
2023-11-09 13:09:57 +0200

    url module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+// npm i url
+
 
 // it will create a web server for us (a baisc serve, without any express or something)
 const myServer = http.createServer((req, res)=>{
@@ -7,16 +10,30 @@ const myServer = http.createServer((req, res)=>{
     console.log("A new request recieved");
     // logging request and then sending response
     const log = `${Date.now()} ": Request recieved from url " ${req.url}\n`;
+    
+    const myUrl = url.parse(req.url)
+    const urlWithParsedQuery = url.parse(req.url,true);
+    console.log(myUrl);
+    console.log(urlWithParsedQuery)
+    console.log(myUrl.pathname);
+    
+
     // it should be non blocking means async
     fs.appendFile("./logs.txt", log, (err, result)=>{
-        switch(req.url){
+        switch(urlWithParsedQuery.pathname){
             case("/"):
             res.end("HomePage");
             //we can send html as well, which we call server side rendering
             break;
 
             case("/about"):
-            res.end("About Us");
+            const name =  urlWithParsedQuery.query.name;
+            res.end(`Hello ${name}`);
+            break;
+
+            case("/search"):
+            const search = urlWithParsedQuery.query.search_query;
+            res.end(`Here are your search results for ${search}`)
             break;
 
             default:
